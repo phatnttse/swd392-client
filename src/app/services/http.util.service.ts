6878ecbabe6b6cfb9d439aa50e2bd1d0ffd136a4
phucpu03fptvn <156656593+phucpu03fptvn@users.noreply.github.com>
2024-10-10 14:39:51 +0700

    forgot-password + profile + reset password

---
+++ src/app/services/http.util.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class HttpUtilService {
+    createHeaders(): HttpHeaders {
+        return new HttpHeaders({
+            'Content-Type': 'application/json',
+        });
+    }
+}
